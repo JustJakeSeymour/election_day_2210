7e7ada928a6d19fd73a2d089abc26baa4a827a18
J Seymour <JustJakeSeymour@gmail.com>
2022-11-01 10:30:44 -0600

    complete building election to spec

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,32 @@
 class Election
+
+  attr_reader :year, 
+              :races
+
+  def initialize(year)
+    @year = year
+    @races = []
+  end
+
+  def add_race(race)
+    @races << race
+  end
+
+  def candidates
+    candidates = []
+    @races.each do |race|
+      race.candidates.select do |candidate|
+        candidates << candidate
+      end
+    end
+    candidates
+  end
+
+  def vote_counts
+    hash = {}
+    candidates.each{|candidate|
+    hash[candidate.name] = candidate.votes
+    }
+    hash
+  end
 end
