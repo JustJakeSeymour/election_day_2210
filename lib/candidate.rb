8180e5f0f348ec777b1e22422b0603a18512327d
J Seymour <JustJakeSeymour@gmail.com>
2022-11-01 09:16:34 -0600

    build candidate file to spec

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,21 @@
 class Candidate
+
+  attr_reader :info, :votes
+
+  def initialize(info)
+    @info = info
+    @votes = 0
+  end
+
+  def name
+    self.info[:name]
+  end
+  
+  def party
+    self.info[:party]
+  end
+
+  def vote_for!
+    @votes += 1
+  end
 end
